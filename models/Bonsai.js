4f13e5d4ce45c317ae935391627a7819a5f963c8
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 16:47:31 -0700

    Make attribute props optional

--- models/Bonsai.js
+++ models/Bonsai.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const BonsaiSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   bonsaiChapters: [
-    { type: Schema.Types.ObjectId, ref: "BonsaiChapter", required: true },
+    { type: Schema.Types.ObjectId, ref: 'BonsaiChapter', required: true }
   ],
   hardiness_zone: { type: String, maxLength: 200, required: true },
-  height: { type: String, maxLength: 200, required: true },
-  width: { type: String, maxLength: 200, required: true },
-  nebari: { type: String, maxLength: 200, required: true },
-  style: { type: String, maxLength: 200, required: true },
-  species: { type: String, maxLength: 200, required: true },
+  height: { type: String, maxLength: 200 },
+  width: { type: String, maxLength: 200 },
+  nebari: { type: String, maxLength: 200 },
+  style: { type: String, maxLength: 200 },
+  species: { type: String, maxLength: 200, required: true }
 });
 
-module.exports = mongoose.model("BonsaiChapter", BonsaiSchema);
+module.exports = mongoose.model('BonsaiChapter', BonsaiSchema);
