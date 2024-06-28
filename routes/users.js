b0bc9d03ea19760e53d8c033b793a978c43ca4b3
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 16:58:03 -0700

    Create user router Partially implement post route. (Actually). Bonsai post route not implemented.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Get user. */
+router.get('/:id', function (req, res, next) {
+  res.send('get user');
+});
+
+/* Create user. */
+router.post('/', async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const newUser = new User({ username });
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (error) {
+    console.error('Error saving user:', error);
+    res.status(500).send({ error: 'Failed to save user' });
+  }
+});
+
+/* Update user. */
+router.put('/:id', function (req, res, next) {
+  res.send('update user');
+});
+
+/* Delete user */
+router.delete('/:id', function (req, res, next) {
+  res.send('delete user');
 });
 
 module.exports = router;
