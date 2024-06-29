7c672e17f729bfbc820b85f5e61d7f40545fbf5a
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 17:04:46 -0700

    Update route name

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-require("dotenv").config();
-require("./mongoConfig");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config();
+require('./mongoConfig');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var bonsaiRouter = require('./routes/bonsai.js');
+var usersRouter = require('./routes/users.js');
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/bonsai', bonsaiRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
