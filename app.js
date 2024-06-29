3adc8848ad0e93c6b5687232e7949b7d9cca775b
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 17:21:16 -0700

    Replace all var with const Remnants of express generator.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
 require('./mongoConfig');
 
-var bonsaiRouter = require('./routes/bonsai.js');
-var usersRouter = require('./routes/users.js');
+const bonsaiRouter = require('./routes/bonsai.js');
+const usersRouter = require('./routes/users.js');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/bonsai.js
+++ routes/bonsai.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* Get single bonsai. TODO*/
 router.get('/:id', function (req, res, next) {
