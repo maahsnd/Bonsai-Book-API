4c0b95e3b75bb4dba0fe175e6866ed9e59c7a831
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 17:06:49 -0700

    Use only if env is testing

--- mongoConfig.js
+++ mongoConfig.js
@@ -1,8 +1,13 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-main().catch((err) => console.log(err));
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+if (process.env.NODE_ENV !== 'test') {
+  main().catch((err) => console.log(err));
+}
+
 async function main() {
   try {
+    console.log('Connecting to MongoDB...');
     await mongoose.connect(process.env.MONGO_STR);
     console.log(`Successfully connected to MONGO`);
   } catch (error) {
