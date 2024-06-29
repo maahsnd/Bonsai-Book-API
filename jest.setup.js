fbc86135b599782e60aabd7ee786fdaf4f81036f
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-28 17:05:14 -0700

    Create jest setup

---
+++ jest.setup.js
@@ -0,0 +1,17 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+  if (mongoose.connection.readyState === 1) {
+    await mongoose.connection.close();
+  }
+  await mongoServer.stop();
+});
