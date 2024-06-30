6462e859225ce44b29c3447a98e2148e81995833
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-30 12:27:21 -0700

    Create test for user fetch

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,12 +2,25 @@ const request = require('supertest');
 const app = require('../app.js');
 
 describe('User API', () => {
-  it('should create a new user', async () => {
+  let userId; 
+
+  it('should create a new user (required fields only)', async () => {
     const res = await request(app).post('/users/').send({
-      username: 'testuser'
+      username: 'testuser',
+      email: 'test@email.com',
+      passwordHash: 'testpassword',
     });
     expect(res.statusCode).toEqual(201);
     expect(res.body).toHaveProperty('_id');
     expect(res.body.username).toEqual('testuser');
+    userId = res.body._id; 
+  });
+
+  it('should fetch a user (required fields only)', async () => {
+    const res = await request(app).get(`/users/${userId}`).send();
+    expect(res.statusCode).toEqual(200);  
+    expect(res.body._id).toEqual(userId);
+    expect(res.body.username).toEqual('testuser');
+    expect(res.body.email).toEqual('test@email.com');
   });
 });
