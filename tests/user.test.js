2b63efd9171fbe507f8a189dac8aea9292ff3486
mrwindupbirdesq <nmaahs11@gmail.com>
2024-06-30 12:33:40 -0700

    Create test for edit and delete user

--- tests/user.test.js
+++ tests/user.test.js
@@ -23,4 +23,21 @@ describe('User API', () => {
     expect(res.body.username).toEqual('testuser');
     expect(res.body.email).toEqual('test@email.com');
   });
+  it('should edit a user (required fields only)', async () => {
+    const res = await request(app).put(`/users/${userId}`).send({
+      username : 'bestuser',
+      email : 'best@email.com'
+    });
+    expect(res.statusCode).toEqual(200);  
+    expect(res.body._id).toEqual(userId);
+    expect(res.body.username).toEqual('bestuser');
+    expect(res.body.email).toEqual('best@email.com');
+  });
+  it('should delete a user ', async () => {
+    const res = await request(app).delete(`/users/${userId}`).send();
+    expect(res.statusCode).toEqual(200);  
+    expect(res.body._id).toEqual(userId);
+    const res1 = await request(app).get(`/users/${userId}`).send();
+    expect(res1.statusCode).toEqual(404);
+  });
 });
